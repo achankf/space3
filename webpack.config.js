177462a54f50b707f5ee27e9bf12ab8063acf86b
Alfred Chan <alfred8844@gmail.com>
2020-10-10 00:14:09 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const distPath = path.resolve(__dirname, "dist");
+module.exports = (env, argv) => {
+  return {
+    devServer: {
+      contentBase: distPath,
+      compress: argv.mode === 'production',
+      port: 8000
+    },
+    entry: './bootstrap.js',
+    output: {
+      path: distPath,
+      filename: "[name].[contenthash].js",
+      webassemblyModuleFilename: "space3.wasm"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: './static', to: distPath }
+      ]),
+      new WasmPackPlugin({
+        crateDirectory: ".",
+        extraArgs: "--no-typescript",
+      }),
+      new HtmlWebpackPlugin({
+        template: './static/index.html'
+      }),
+    ],
+    watch: argv.mode !== 'production'
+  };
+};
